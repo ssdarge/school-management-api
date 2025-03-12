b3f3a2a6797fbb8b3b3ca199ebee9f403f421a18
swapnil <98452773+ssdarge@users.noreply.github.com>
2025-03-13 00:20:12 +0530

    feat: added SSL support for MySQL in db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
-import mysql from "mysql2/promise";
 import fs from "fs";
+import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
