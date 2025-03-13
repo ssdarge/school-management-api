b8d8d782a87aa3973190fa29703f0602d055f294
swapnil <98452773+ssdarge@users.noreply.github.com>
2025-03-13 10:42:47 +0530

    update: MySQL connection pool

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ import "dotenv/config";
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 28805,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
